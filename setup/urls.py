9a0df84b3bb16af2bdc47959db0ec7e3ee0c7505
eriveltoSilva <eriveltoclenio@gmail.com>
2024-07-02 18:45:19 +0100

    first commit

---
+++ setup/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for setup project.
+The `urlpatterns` list routes URLs to views. 
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
+]
