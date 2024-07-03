f5876c2e1c1ed292e8aa9d27a8e8c548e93b3379
eriveltoSilva <eriveltoclenio@gmail.com>
2024-07-03 02:25:30 +0100

    feat: relationship serializer field type

--- apps/courses/serializers.py
+++ apps/courses/serializers.py
@@ -1,13 +1,6 @@
 from rest_framework import serializers
 from .models import Course, Review
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = (
-            'id', 'title', 'url', 'created_at', 'is_active'
-        )
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         extra_kwargs = {
@@ -20,3 +13,20 @@ class ReviewSerializer(serializers.ModelSerializer):
             'email', 'comment', 'review', 
             'created_at', 'is_active'
         )
+
+class CourseSerializer(serializers.ModelSerializer):
+    # 1. Nested Relationship
+    # reviews = ReviewSerializer(many=True, read_only=True)
+
+    # 2. HyperLinked Related Field
+    reviews = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='review')
+
+    # 3. Primary key Related Field
+    # reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            'id', 'title', 'url', 'created_at', 'is_active', 'reviews'
+        )
+    
--- setup/settings.py
+++ setup/settings.py
@@ -113,8 +113,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#     ]
-# }
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
+}
